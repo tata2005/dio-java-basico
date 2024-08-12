cb84690963b0dc96c0f0884046452f4a16ece673
Tarciso Costa <tarcisocosta3467@gmail.com>
2024-08-12 16:05:01 -0300

    tipos de variaves

--- TiposVariaveis/src/App.java
+++ TiposVariaveis/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        double salarioMinimo = 2.500;
+        short numeroCurto = 1;
+        int numeroNormal = 12;
+        final double NUMERO_DE_PI = 3.14;
     }
 }
